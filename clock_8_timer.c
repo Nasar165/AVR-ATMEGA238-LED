0fe44c40d61f84f32e55bb8919c891acdded2cc4
Nasar Eddaoui <Nasar165@hotmail.com>
2021-04-07 00:32:19 +0700

    missing or operator

--- clock_8_timer.c
+++ clock_8_timer.c
@@ -17,13 +17,13 @@ int interrupts = 0; // count number of interrupts triggered
 
 int main(void) {
     DDRD |= (1<<PD7);       // Data direction output PD7
-    TCCR0A = (1 <<WGM01);   // Set Waveform Generation to CTC
+    TCCR0A |= (1 <<WGM01);   // Set Waveform Generation to CTC
     OCR0A = 156;            // Set OCR0A to 156 Total timer ticks MAX 255 8Bit
     TIMSK0 |= (1 << OCIE0A); // Set interrupt Register enable 
     
     sei();                   // Enable interrupts
     
-    TCCR0B = (1 << CS02) | (1 << CS00); // enable timer clock
+    TCCR0B |= (1 << CS02) | (1 << CS00); // enable timer clock
     while (1) {
         
     }
